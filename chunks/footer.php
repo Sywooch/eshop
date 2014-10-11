436d529c52e3141442316d662f24207b1f9eacf1
earthperson <ponomarev.dev@gmail.com>
2014-10-11 21:47:47 +0400

    v0.3.2-pl

--- chunks/footer.php
+++ chunks/footer.php
@@ -2,7 +2,7 @@
 			<div class="container">
 				<p class="text-muted">
 					<small class="copy">&copy; <?php echo date('Y') > 2014 ? date('Y') . '-' : '' ?>2014</small> <a href="https://github.com/earthperson/eshop" target="_blank" class="small"><?php echo CHtml::encode(Yii::app()->name) ?></a> <a href="public-offer.php" class="small">Публичная оферта</a>, <a href="feedback.php" class="small">Обратная связь</a>
-					<small class="pull-right">&nbsp;v0.3.1-pl</small><a href="<?php echo Yii::app()->params['VK.group']; ?>" target="_blank" class="pull-right small">Группа ВКонтакте</a><span class="pull-right">,&nbsp;</span><a href="http://dev.earthperson.info" target="_blank" class="pull-right small">Разработка</a>
+					<small class="pull-right">&nbsp;v0.3.2-pl</small><a href="<?php echo Yii::app()->params['VK.group']; ?>" target="_blank" class="pull-right small">Группа ВКонтакте</a><span class="pull-right">,&nbsp;</span><a href="http://dev.earthperson.info" target="_blank" class="pull-right small">Разработка</a>
 				</p>
 			</div>
 		</div>

598171aea684b453b53e8df8c8e8cc7af95ce064
earthperson <ponomarev.dev@gmail.com>
2015-11-07 23:46:39 +0400

    i18n

--- views/feedback.php
+++ views/feedback.php
@@ -6,15 +6,15 @@ $model->setAttributes($_POST);
 ?>
 <div class="container">
 	<div class="page-header">
-		<h1><?php echo \Yii::t('app', 'Обратная связь') ?></h1>
+		<h1><?= \Yii::t('app', 'Обратная связь') ?></h1>
 	</div>
 	<?php if(empty($_POST) or !$model->validate()) { ?>
-	<p>Способы обратной связи:</p>
+	<p><?= \Yii::t('app', 'Способы обратной связи:') ?></p>
 	<ol>
-		<li>Воспользоваться формой обратной связи на этой странице;</li>
-		<li>Написать сообщение администраторам <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><img src="images/vk.com.jpg" width="16" height="16" alt="" title="Группа ВКонтакте"></a> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank">группы ВКонтакте</a>;</li>
-		<li>Позвонить по телефону <em><?= Yii::$app->params['phone'] ?></em>;</li>
-		<li>Написать письмо на почту <?= Yii::$app->params['email'] ?> в свободной форме с указанием Ваших координат и ФИО.</li>
+		<li><?= \Yii::t('app', 'Воспользовавшись формой обратной связи на этой странице') ?>;</li>
+		<li><?= \Yii::t('app', 'Написать сообщение администраторам') ?> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><img src="images/vk.com.jpg" width="16" height="16" alt="" title="<?= \Yii::t('app', 'Группа ВКонтакте') ?>"></a> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><?= \Yii::t('app', 'группы ВКонтакте') ?></a>;</li>
+		<li><?= \Yii::t('app', 'Позвонить по телефону') ?> <em><?= Yii::$app->params['phone'] ?></em>;</li>
+		<li><?= \Yii::t('app', 'Написать письмо на почту {0} в свободной форме с указанием Ваших координат и ФИО', Yii::$app->params['email']) ?>.</li>
 	</ol>
 	<div class="well">
 		<h3 class="text-info"><?php echo \Yii::t('app', 'Форма обратной связи') ?></h3>
@@ -33,30 +33,30 @@ $model->setAttributes($_POST);
 		<form role="form" action="" method="post">
 			<input type="hidden" name="nospam" value="">
   			<div class="form-group<?= isset($errors['fio']) ? ' has-error' : '' ?>">
-  				<label class="control-label" for="controlFio">ФИО<small></small></label>
+  				<label class="control-label" for="controlFio"><?= \Yii::t('app', 'ФИО') ?><small></small></label>
 				<input type="text" class="form-control" name="fio" id="controlFio" value="">
 			</div>
 			<div class="form-group<?= isset($errors['phone']) ? ' has-error' : '' ?>">
-				<label class="control-label" for="controlPhone">Телефон<small></small></label>
+				<label class="control-label" for="controlPhone"><?= \Yii::t('app', 'Телефон') ?><small></small></label>
 				<input type="tel" class="form-control" name="phone" id="controlPhone" value="">
 			</div>
 			<div class="form-group<?= isset($errors['email']) ? ' has-error' : '' ?>">
-				<label class="control-label" for="controlEmail">Email<small></small></label>
+				<label class="control-label" for="controlEmail"><?= \Yii::t('app', 'Email') ?><small></small></label>
 				<input type="email" class="form-control" name="email" id="controlEmail" value="">
 			</div>
 			<div class="form-group<?= isset($errors['text']) ? ' has-error' : '' ?>">
-				<label class="control-label" for="controlText">Сообщение<small></small></label>
+				<label class="control-label" for="controlText"><?= \Yii::t('app', 'Сообщение') ?><small></small></label>
 				<textarea class="form-control" name="text" id="controlText" rows="4"></textarea>
 			</div>
 			<div class="form-group<?= isset($errors['math']) ? ' has-error' : '' ?>">
-				<label class="control-label" for="controlMath"><small>Защита от спама, напишите сколько будет: <?= $math_init['op1'] ?> <?= $math_init['operator'] ? 'плюс' : 'минус' ?> <?= $math_init['op2'] ?>?</small></label>
+				<label class="control-label" for="controlMath"><small><?= \Yii::t('app', 'Защита от спама, напишите сколько будет: {0, number} {1} {2, number}?', [$math_init['op1'], $math_init['operator'] ? \Yii::t('app', 'плюс')  : \Yii::t('app', 'минус'), $math_init['op2']]) ?></small></label>
 				<input type="number" class="form-control" name="math" id="controlMath" value="">
 			</div>
-			<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-send"></span> Отправить</button>
+			<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-send"></span> <?= \Yii::t('app', 'Отправить') ?></button>
 		</form>
 	</div>
 	<?php } else { ?>
-	<div class="alert alert-success">Спасибо! Обратная связь с Клиентами помогает улучшить наш сервис.</div>
+	<div class="alert alert-success"><?= \Yii::t('app', 'Спасибо! Обратная связь с Клиентами помогает улучшить наш сервис.') ?></div>
 	<?php 
 		/* Yii::$app->mailer->compose()
 		->setFrom()
--- views/how-to-buy.php
+++ views/how-to-buy.php
@@ -4,30 +4,27 @@ use app\helpers\EshopHelper;
 ?>
 <div class="container">
 	<div class="page-header">
-		<h1>Где и как купить футболку</h1>
+		<h1><?= \Yii::t('app', 'Где и как купить футболку') ?></h1>
 	</div>
-	<p>Приобрести футболку можно следующими способами:</p>
+	<p><?= \Yii::t('app', 'Приобрести футболку можно следующими способами:') ?></p>
 	<ol>
-		<li>Воспользовавшись этим сайтом (подробные инструкции см. ниже);</li>
-		<li>Написав сообщение администраторам <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><img src="images/vk.com.jpg" width="16" height="16" alt="" title="Группа ВКонтакте"></a> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank">группы ВКонтакте</a>;</li>
-		<li>Позвонив по телефону <em><?= Yii::$app->params['phone'] ?></em>;</li>
-		<li>Написав письмо на почту <?= Yii::$app->params['email'] ?> в свободной форме с указанием Ваших координат и ФИО.</li>
+		<li><?= \Yii::t('app', 'Воспользовавшись этим сайтом (подробные инструкции см. ниже)') ?>;</li>
+		<li><?= \Yii::t('app', 'Написать сообщение администраторам') ?> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><img src="images/vk.com.jpg" width="16" height="16" alt="" title="<?= \Yii::t('app', 'Группа ВКонтакте') ?>"></a> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><?= \Yii::t('app', 'группы ВКонтакте') ?></a>;</li>
+		<li><?= \Yii::t('app', 'Позвонить по телефону') ?> <em><?= Yii::$app->params['phone'] ?></em>;</li>
+		<li><?= \Yii::t('app', 'Написать письмо на почту {0} в свободной форме с указанием Ваших координат и ФИО', Yii::$app->params['email']) ?>.</li>
 	</ol>
-	<p>Отзывы о нас читайте на стене <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><img src="images/vk.com.jpg" width="16" height="16" alt="" title="Группа ВКонтакте"></a> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank">группы ВКонтакте</a>.</p>
-	<h2>Подробные инструкции по использованию сайта</h2>
-	<p><span class="label label-info">Внимание!</span> Вы можете просмотреть <a href="#" class="how-to-buy-video"><span class="glyphicon glyphicon-flag"></span> видео</a> и <a href="#" class="how-to-buy-gallery"><span class="glyphicon glyphicon-flag"></span> галерею скриншотов</a> процесса покупки.</p>
+	<p><?= \Yii::t('app', 'Отзывы о нас читайте на стене') ?> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><img src="images/vk.com.jpg" width="16" height="16" alt="" title="<?= \Yii::t('app', 'Группа ВКонтакте') ?>"></a> <a href="<?= Yii::$app->params['VK.group'] ?>" target="_blank"><?= \Yii::t('app', 'группы ВКонтакте') ?></a>.</p>
+	<h2><?= \Yii::t('app', 'Подробные инструкции по использованию сайта') ?></h2>
+	<p><span class="label label-info"><?= \Yii::t('app', 'Внимание!') ?></span> <?= \Yii::t('app', 'Вы можете просмотреть') ?> <a href="#" class="how-to-buy-video"><span class="glyphicon glyphicon-flag"></span> <?= \Yii::t('app', 'видео') ?></a> <?= \Yii::t('app', 'и') ?> <a href="#" class="how-to-buy-gallery"><span class="glyphicon glyphicon-flag"></span> <?= \Yii::t('app', 'галерею скриншотов') ?></a> <?= \Yii::t('app', 'процесса покупки.') ?></p>
 	<ul>
-		<li>Положите футболки <a href="<?= EshopHelper::createUrl('cart') ?>">в корзину</a> выбрав те, которые Вам нравятся. Для футболок относящихся к хитам продаж 
-		вы можете листая влево и вправо список вверху главной страницы указать количество и размер и затем нажать на кнопку «Положить в корзину». Остальные футболки можно положить
-		одним нажатем на кнопку «Положить в корзину», а количество и размер отредактировать уже в корзине;</li>
-		<li>Просмотрите и в случае необходимости обновите корзину нажимая кнопки «<span class="glyphicon glyphicon-pencil"></span> Обновить» или «<span class="glyphicon glyphicon-remove"></span> Удалить» позицию. Здесь же Вы можете отредактировать надпись на футболке нажав на <span class="glyphicon glyphicon-pencil"></span> в колонке «Надпись» напротив каждой позиции
-		 и указать хотите ли Вы, чтобы в правом нижнем углу футболки была ссылка на группу ВКонтакте (это дает <em>10%</em> скидку) отметив соответствующий чекбокс в колонке «Ссылка на группу» напротив каждой позиции. Ваши футболки хранятся в корзине в течении одного дня;</li>
-		<li>Нажмите «Оформить заказ»;</li>
-		<li>Заполните свои контактные данные, ФИО и желательное время для звонка менеджера в случае доставки курьером;</li>
-		<li>Нажмите «Отправить заказ» и Ваш заказ будет отправлен менеджеру. В случае, если Вы верно указали свои контактные данные и ФИО менеджер перезвонит или отпишется Вам в удобное для Вас время;</li>
-		<li>Также вы можете оформить заказ у наших партнеров и распространителей флаеров;</li>
-		<li>Вы можете внести предоплату через этот сайт. Подробнее читайте на странице <a href="<?= EshopHelper::createUrl('payment') ?>">Способы оплаты</a>.</li>
-		<li>Спасибо, что выбрали футболки от  <?= Html::encode(Yii::$app->name) ?>. В этих футболках у Вас всегда будет хорошее настроение!</li>
+		<li><?= \Yii::t('app', 'Положите футболки <a href="{0}">в корзину</a> выбрав те, которые Вам нравятся. Для футболок относящихся к хитам продаж вы можете листая влево и вправо список вверху главной страницы указать количество и размер и затем нажать на кнопку «Положить в корзину». Остальные футболки можно положить одним нажатем на кнопку «Положить в корзину», а количество и размер отредактировать уже в корзине;', EshopHelper::createUrl('cart')) ?></li>
+		<li><?= \Yii::t('app', 'Просмотрите и в случае необходимости обновите корзину нажимая кнопки «<span class="glyphicon glyphicon-pencil"></span> Обновить» или «<span class="glyphicon glyphicon-remove"></span> Удалить» позицию. Здесь же Вы можете отредактировать надпись на футболке нажав на <span class="glyphicon glyphicon-pencil"></span> в колонке «Надпись» напротив каждой позиции и указать хотите ли Вы, чтобы в правом нижнем углу футболки была ссылка на группу ВКонтакте (это дает <em>10%</em> скидку) отметив соответствующий чекбокс в колонке «Печатать ссылку» напротив каждой позиции. Ваши футболки хранятся в корзине в течении одного дня;') ?></li>
+		<li><?= \Yii::t('app', 'Нажмите «Оформить заказ»;') ?></li>
+		<li><?= \Yii::t('app', 'Заполните свои контактные данные, ФИО и желательное время для звонка менеджера в случае доставки курьером;') ?></li>
+		<li><?= \Yii::t('app', 'Нажмите «Отправить заказ» и Ваш заказ будет отправлен менеджеру. В случае, если Вы верно указали свои контактные данные и ФИО менеджер перезвонит или отпишется Вам в удобное для Вас время;') ?></li>
+		<li><?= \Yii::t('app', 'Также вы можете оформить заказ у наших партнеров и распространителей флаеров;') ?></li>
+		<li><?= \Yii::t('app', 'Вы можете внести предоплату через этот сайт. Подробнее читайте на странице <a href="{0}">способы оплаты</a>.', EshopHelper::createUrl('payment')) ?></li>
+		<li><?= \Yii::t('app', 'Спасибо, что выбрали футболки от {0}. В этих футболках у Вас всегда будет хорошее настроение!', Html::encode(Yii::$app->name)) ?></li>
 	</ul>
 </div>
 <div id="links" style="display: none;">
@@ -41,18 +38,18 @@ use app\helpers\EshopHelper;
 		<div class="modal-content">
 			<div class="modal-header">
 				<button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title" id="modalScreencastLabel">Screencast</h4>
+				<h4 class="modal-title" id="modalScreencastLabel"><?= \Yii::t('app', 'Галерея скриншотов') ?></h4>
 			</div>
 			<div class="modal-body">
 				<video controls width="800" height="600" poster="http://earthperson.github.io/eshop/images/presentation/2.png">
 					<source src="http://earthperson.github.io/eshop/images/presentation/screencast/eshop.webm" type="video/webm">
 					<source src="http://earthperson.github.io/eshop/images/presentation/screencast/eshop.mp4" type="video/mp4">
 					<source src="http://earthperson.github.io/eshop/images/presentation/screencast/eshop.ogv" type="video/ogv">
-					I'm sorry; your browser doesn't support HTML5 video in WebM with VP8 or MP4 with H.264.
+					<?= \Yii::t('app', 'Очень жаль, но Ваш браузер не поддерживает HTML5 видео в формате WebM с VP8 или MP4 с H.264.') ?>
 				</video>
 			</div>
 			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal"><?= \Yii::t('app', 'Закрыть') ?></button>
 			</div>
 		</div><!-- /.modal-content -->
 	</div><!-- /.modal-dialog -->
@@ -80,10 +77,10 @@ use app\helpers\EshopHelper;
 				<div class="modal-footer">
                     <button type="button" class="btn btn-default pull-left prev">
                         <i class="glyphicon glyphicon-chevron-left"></i>
-                        Предыдущее
+                        	<?= \Yii::t('app', 'Предыдущее') ?>
                     </button>
                     <button type="button" class="btn btn-primary next">
-                        Следующее
+                        	<?= \Yii::t('app', 'Следующее') ?>
                         <i class="glyphicon glyphicon-chevron-right"></i>
                     </button>
                 </div>
